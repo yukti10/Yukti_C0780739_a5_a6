1492f8ababff7ab2651074451511e82b37d952a1
yukti10 <ysaluja10@gmail.com>
2021-03-05 18:14:50 -0500

    initial commit

--- src/Yukti_C0780739_a5/assignment5.java
+++ src/Yukti_C0780739_a5/assignment5.java
@@ -4,5 +4,21 @@ public class assignment5 {
     public static void main(String[] args){
 
     }
-    public static
+    public class services_cost{
+         double packageA;
+         double packageB;
+    }
+    public class options_cost{
+         double storageSmall;
+         double storageLarge;
+    }
+    public class items_cost{
+        double boxesSmall;
+        double boxesLarge;
+
+    }
+
+    public static void total_cost(){
+
+    }
 }
