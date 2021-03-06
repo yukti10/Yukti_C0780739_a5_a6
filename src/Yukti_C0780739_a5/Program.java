b154bb5294acebb845afaa1eaa1953d0b247af34
yukti10 <ysaluja10@gmail.com>
2021-03-06 18:45:25 -0500

    Final commit

---
+++ src/Yukti_C0780739_a5/Program.java
@@ -0,0 +1,58 @@
+package Yukti_C0780739_a5;
+
+public class Program {
+    private double packageAService;
+    private double packageBService;
+    private double storageSmall;
+    private double storageLarge;
+    private double boxesSmall;
+    private double boxesLarge;
+
+    public double getPackageAService() {
+        return packageAService;
+    }
+
+    public void setPackageAService(double packageAService) {
+        this.packageAService = packageAService;
+    }
+
+    public double getPackageBService() {
+        return packageBService;
+    }
+
+    public void setPackageBService(double packageBService) {
+        this.packageBService = packageBService;
+    }
+
+    public double getStorageSmall() {
+        return storageSmall;
+    }
+
+    public void setStorageSmall(double storageSmall) {
+        this.storageSmall = storageSmall;
+    }
+
+    public double getStorageLarge() {
+        return storageLarge;
+    }
+
+    public void setStorageLarge(double storageLarge) {
+        this.storageLarge = storageLarge;
+    }
+
+    public double getBoxesSmall() {
+        return boxesSmall;
+    }
+
+    public void setBoxesSmall(double boxesSmall) {
+        this.boxesSmall = boxesSmall;
+    }
+
+    public double getBoxesLarge() {
+        return boxesLarge;
+    }
+
+    public void setBoxesLarge(double boxesLarge) {
+        this.boxesLarge = boxesLarge;
+    }
+}
---
+++ src/Yukti_C0780739_a5/main.java
@@ -0,0 +1,122 @@
+package Yukti_C0780739_a5;
+
+import javax.swing.*;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.*;
+
+public class main {
+
+    static boolean Login() {
+        int count = 0;
+        Boolean flag = false;
+        while (count < 3) {
+            JFrame f = new JFrame();
+            String username = JOptionPane.showInputDialog(f, "Enter UserName:");
+            String password = JOptionPane.showInputDialog(f, "Enter Password:");
+            if (username.equals("Yukti") && password.equals("Password")) {
+                JOptionPane.showMessageDialog(f, "User logged in successfully.");
+                flag = true;
+                break;
+            } else {
+                JOptionPane.showMessageDialog(f, "Incorrect credentials.");
+                if (count == 2) ;
+                JOptionPane.showMessageDialog(f, "You have exceeded the number of attempts");
+
+            }
+            count++;
+        }
+        return flag;
+    }
+
+    static void options() {
+        if (Login() == true) {
+            JPanel jp = new JPanel();
+            JTextField fName = new JTextField(5);
+            JTextField lName = new JTextField(5);
+            JCheckBox servicesPackageA = new JCheckBox("Package A - $100/hr");
+            servicesPackageA.setBounds(100, 100, 50, 50);
+            JCheckBox servicesPackageB = new JCheckBox("Package B – $150/hr");
+            servicesPackageB.setBounds(100, 100, 50, 50);
+            JCheckBox optionsStorageSmall = new JCheckBox("Storage Small – $8/day");
+            optionsStorageSmall.setBounds(100, 100, 50, 50);
+            JCheckBox optionsStorageLarge = new JCheckBox("Storage Large – $20.11/day");
+            optionsStorageLarge.setBounds(100, 100, 50, 50);
+            JCheckBox itemsBoxesSmall = new JCheckBox("Boxes small - $2.50/per box");
+            itemsBoxesSmall.setBounds(100, 100, 50, 50);
+            JCheckBox itemsBoxesLarge = new JCheckBox("Boxes large - $4.50/ per box");
+            itemsBoxesLarge.setBounds(100, 100, 50, 50);
+            jp.add(new JLabel("First Name:"));
+            jp.add(fName);
+            jp.add(Box.createHorizontalStrut(15));
+            jp.add(new JLabel("Last Name:"));
+            jp.add(lName);
+            JOptionPane.showConfirmDialog(null, jp,
+                    "Please Enter below details", JOptionPane.OK_CANCEL_OPTION);
+            JPanel jp1 = new JPanel();
+            jp1.add(new JLabel("Services"));
+            jp1.add(servicesPackageA);
+            jp1.add(servicesPackageB);
+            jp1.add(new JLabel("Options\n"));
+            jp1.add(optionsStorageSmall);
+            jp1.add(optionsStorageLarge);
+            jp1.add(new JLabel("Items\n"));
+            jp1.add(itemsBoxesSmall);
+            jp1.add(itemsBoxesLarge);
+            JOptionPane.showConfirmDialog(null, jp1,
+                    "Please Enter below details", JOptionPane.OK_CANCEL_OPTION);
+            Program p1 = new Program();
+            p1.setPackageAService(100.0);
+            p1.setPackageBService(150.0);
+            p1.setStorageSmall(8.0);
+            p1.setStorageLarge(20.11);
+            p1.setBoxesSmall(2.5);
+            p1.setBoxesLarge(4.5);
+            double total =0;
+            double serviceCost=0;
+            double itemCost=0;
+            double optionsCost =0;
+            if (servicesPackageA.isSelected()==true){
+                total += p1.getPackageAService();
+                serviceCost+=p1.getPackageAService();
+            }
+            if (servicesPackageB.isSelected()==true){
+                total += p1.getPackageBService();
+                serviceCost+=p1.getPackageBService();
+            }
+            if (optionsStorageSmall.isSelected()==true){
+                total += p1.getStorageSmall();
+                optionsCost+=p1.getStorageSmall();
+
+            }
+            if (optionsStorageLarge.isSelected()==true){
+                total += p1.getStorageLarge();
+                optionsCost+=p1.getStorageLarge();
+            }
+            if (itemsBoxesSmall.isSelected()==true){
+                total += p1.getBoxesSmall();
+                itemCost+=p1.getBoxesSmall();
+            }
+            if (itemsBoxesLarge.isSelected()==true){
+                total += p1.getBoxesLarge();
+                itemCost+=p1.getBoxesLarge();
+            }
+
+            JFrame jf = new JFrame();
+            JOptionPane.showMessageDialog(jf,"Your total amount is:"+total);
+            //ASSIGNMENT 6
+            JFrame jf1 = new JFrame();
+            JOptionPane.showMessageDialog(jf1,"Your Service cost is $"+serviceCost +"\n Your Options cost is $"+optionsCost
+            +"\n Your item cost is $"+itemCost+"Your total cost including everything is $"+total);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+            options();
+
+    }
+}
+
